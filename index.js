ba0d5ef4772b5a7f14fb97398bc4b4bed99e7057
Shakar <5h4k4r.b4kr@gmail.com>
2023-09-17 14:12:48 +0300

    Update index.js
    Signed-off-by: Shakar <5h4k4r.b4kr@gmail.com>

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const core = require('@actions/core')
 const github = require('@actions/github')
 const fs = require('fs')
+const xml2js = require('xml2js')
 try {
   const label = core.getInput('label');
   console.log(`Label: ${label}`)
