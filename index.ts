2ca30dd8d21bccd97cbae6064b598c96d11df391
Shakar <5h4k4r.b4kr@gmail.com>
2023-09-17 13:46:26 +0300

    rename index.js to ts
    Signed-off-by: Shakar <5h4k4r.b4kr@gmail.com>

--- index.ts
+++ index.ts
@@ -1,13 +1,20 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-
-
+import * as core from '@actions/core'
+import * as github from '@actions/github'
+import * as fs from 'fs';
 try {
   const label = core.getInput('label');
   console.log(`Label: ${label}`)
 
   core.setOutput("label", label);
 
+
+  fs.readFile('/Readme.md', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(`File data: ${data}`);
+  });
   const payload = JSON.stringify(github.context.payload, undefined, 2)
 
   console.log(`The event payload: ${payload}`);
