96ad9f4977259e2466885716737b5b1b25361a98
Shakar Bakr <5h4k4r.b4kr@gmail.com>
2023-10-04 11:31:21 +0300

    Update logs
    Signed-off-by: Shakar Bakr <5h4k4r.b4kr@gmail.com>

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ async function run() {
     const patterns = ['**/*.csproj', '**/package.json']
     const globber = await glob.create(patterns.join('\n'))
     const files = await globber.glob()
-    console.log(files)
+    console.log("files: ", files)
 
     const filePathInput = core.getInput('filePath');
     const labelInput = core.getInput('label');
